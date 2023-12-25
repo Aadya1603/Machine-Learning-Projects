03d7a30d74f46d440c18785146a0de0ca91bfb27
Aadya Madankar <114391105+Aadya1603@users.noreply.github.com>
2023-12-25 19:38:56 +0530

    Update chat.py

--- End-To-End-Gemini-Project-main/chat.py
+++ End-To-End-Gemini-Project-main/chat.py
@@ -25,14 +25,27 @@ st.set_page_config(page_title="Q&A Demo")
 st.header("Gemini LLM Application")
 
 
-input=st.text_input ("Input: ",key="input")
-submit=st.button("Ask the question")
-## When submit is clicked
-if submit:
-    response=get_gemini_response(input)
-    st.subheader("The Response is")
-    st.write(response)
+# Initialize session state for chat history if it doesn't exist
 
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask the question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
+    
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
 
 
 
